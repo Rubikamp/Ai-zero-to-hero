f5bbdcc0b15ccde7215976f32a0449d22ad89e08
Mehrab HP <white_joker20@yahoo.com>
2024-12-08 21:00:38 +0300

    fix: raw update to use json object

--- chat-with-website-laravel/app/Livewire/Parking/Index.php
+++ chat-with-website-laravel/app/Livewire/Parking/Index.php
@@ -12,7 +12,7 @@ class Index extends Component
     public string $response = '...';
 
     public array $messages = [
-        ['role' => 'system', 'content' => 'Extarct required information from the user message'],
+        ['role' => 'system', 'content' => 'Extarct required information from the user message. return in json format with keys of {name, color, number, price}'],
     ];
 
     public array $cars = [];
@@ -20,17 +20,17 @@ class Index extends Component
     public function submit()
     {
         $car_details = $this->extractCarInformation();
-        $car = json_decode($car_details, true);
+        // $car = json_decode($car_details, true);
 
-        $this->response = json_encode($car_details);
+        // $this->response = json_encode($car_details);
 
-        if (empty($car['number'])) {
-            $car['number'] = "R-" . time();
-        }
+        // if (empty($car['number'])) {
+        //     $car['number'] = "R-" . time();
+        // }
 
-        $this->messages[] = ['role' => 'assistant', 'content' =>  json_encode($car)];
+        // $this->messages[] = ['role' => 'assistant', 'content' =>  json_encode($car)];
 
-        $this->cars[$car['number']] = $car;
+        // $this->cars[$car['number']] = $car;
     }
 
     protected function extractCarInformation()
@@ -40,48 +40,79 @@ class Index extends Component
         $result = MetisClient::getClient()->chat()->create([
             'model' => 'gpt-4o-mini',
             'messages' => $this->messages,
-            'tools' => [
-                [
-                    'type' => 'function',
-                    'function' => [
-                        'name' => 'extract_car_information',
-                        'description' => 'Extract car information, from user message',
-                        'parameters' => [
-                            'type' => 'object',
-                            'properties' => [
-                                'name' => [
-                                    'type' => 'string',
-                                    'description' => 'Extract car name, brand name, or model name, or all of them, with car year if exist',
-                                ],
-                                'number' => [
-                                    'type' => 'string',
-                                    'description' => 'Extract car plate number if exist, if not return null',
-                                    "nullable" => true
-                                ],
-                                'color' => [
-                                    'type' => 'string',
-                                    'description' => 'Extract car color if exist, if not return "-"',
-                                ],
-                                'price' => [
-                                    'type' => 'number',
-                                    'description' => 'Extract price',
-                                ],
-                            ],
-                            'required' => ['name', 'number', 'color', 'price'],
-                        ],
-                    ],
-                ]
-            ],
-            // "response_format" => [
-            //     "type" => "json_schema",
-            //     "json_schema" => [
-            //         "number" => "string",
-            //         "name" => "string",
-            //         "color" => "string",
-            //         "price" => "number"
+            // 'tools' => [
+            //     [
+            //         'type' => 'function',
+            //         'function' => [
+            //             'name' => 'extract_car_information',
+            //             'description' => 'Extract car information, from user message',
+            //             'parameters' => [
+            //                 'type' => 'object',
+            //                 'properties' => [
+            //                     'name' => [
+            //                         'type' => 'string',
+            //                         'description' => 'Extract car name, brand name, or model name, or all of them, with car year if exist',
+            //                     ],
+            //                     'number' => [
+            //                         'type' => 'string',
+            //                         'description' => 'Extract car plate number if exist, if not return null',
+            //                         "nullable" => true
+            //                     ],
+            //                     'color' => [
+            //                         'type' => 'string',
+            //                         'description' => 'Extract car color if exist, if not return "-"',
+            //                     ],
+            //                     'price' => [
+            //                         'type' => 'number',
+            //                         'description' => 'Extract price',
+            //                     ],
+            //                 ],
+            //                 'required' => ['name', 'number', 'color', 'price'],
+            //             ],
+            //         ],
+            //     ]
+            // ],
+            "response_format" => ["type" => "json_object"],
+            // 'response_format' => [
+            //     'type' => 'json_object',
+            // ]
+            // 'response_format' => [
+            //     'type' => 'json_schema',
+            //     'json_schema' => [
+            //         'name' => 'math_response',
+            //         'strict' => true,
+            //         'schema' => [
+            //             'type' => 'object',
+            //             'properties' => [
+            //                 'steps' => [
+            //                     'type' => 'array',
+            //                     'items' => [
+            //                         'type' => 'object',
+            //                         'properties' => [
+            //                             'explanation' => [
+            //                                 'type' => 'string'
+            //                             ],
+            //                             'output' => [
+            //                                 'type' => 'string'
+            //                             ]
+            //                         ],
+            //                         'required' => ['explanation', 'output'],
+            //                         'additionalProperties' => false
+            //                     ]
+            //                 ],
+            //                 'final_answer' => [
+            //                     'type' => 'string'
+            //                 ]
+            //             ],
+            //             'required' => ['steps', 'final_answer'],
+            //             'additionalProperties' => false
+            //         ]
             //     ]
             // ]
         ]);
+
+        $this->response = json_encode($result->choices[0]);
+        return $this->response;
 
         // $this->response = json_encode($result, JSON_PRETTY_PRINT);
         return $result->choices[0]->message->toolCalls[0]->function->arguments;
